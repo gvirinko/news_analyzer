7249858872b1fbd1fc5b2e32c6fab44a918c3568
Ira Gnativ <irina.gnativ@gmail.com>
2020-02-21 22:15:59 +0100

    Updates

--- src/about.js
+++
@@ -1,9 +0,0 @@
-import {CommitApi} from './js/modules/CommitApi.js';
-import './js/swiper-custom.js';
-import {commitsTitle} from './js/constants';
-
-// const commitsTitle = document.querySelector('.commits__title');
-
-if (commitsTitle) {
-  new CommitApi().getData();
-}
